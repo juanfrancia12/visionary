e7a8901c9fb145b9ba0d5f49e7d466780e0fb10f
Juan Francia Sánchez <jf2381886@gmail.ocm>
2022-11-07 23:07:27 -0500

    agregado eslint / prettier / husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+const RULES = {
+  OFF: "off",
+  WARN: "warn",
+  ERROR: "error",
+}
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/prop-types": RULES.OFF,
+    "react/react-in-jsx-scope": RULES.OFF,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+}
