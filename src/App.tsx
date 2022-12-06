30331ceab5aaebf02b41d266a8db1ec4229e8506
Juan Francia Sánchez <jf2381886@gmail.ocm>
2022-12-06 02:53:39 -0500

    eliminado importaciones no usadas en app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { AppRoutes } from "@components/routes/AppRoutes"
-// import AppExample from "AppRoute.example"
-// import Layout from "example/Layout"
 import { ToastContainer } from "react-toastify"
 
 function App(): JSX.Element {
   return (
     <>
       <ToastContainer />
-      {/* <AppExample /> */}
       <AppRoutes />
-      {/* <Layout /> */}
     </>
   )
 }
